35c0d8684870ec0ed4e5764c922bba6e35023185
Manuel Schmalstieg <git.code@ms-studio.net>
2020-02-19 23:16:14 +0100

    adding the "Content Child" template

---
+++ template-parts/content-child.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Display Sub-Post Content
+*/
+
+$args = array(
+	    'post_type'      => 'post',
+	    'posts_per_page' => -1,
+	    'post_parent'    => $post->ID,
+	    'order'          => 'ASC',
+	    'orderby'        => 'date'
+	 );
+	
+	$children = new WP_Query( $args );
+	
+	$children_count = $children->post_count;
+		
+	if ( $children->have_posts() ) : ?>
+	
+	<?php 
+	
+	// Define counter:
+	$post_count = 1;
+	
+	 ?>
+		
+	<section class="child-posts">
+		<header class="child-posts-header section-inner">
+			<h1 class="wrapper"><?php
+			
+			echo sprintf( _n( '%s report for ', '%s reports for ', $children_count, 'designbriefs' ), $children_count );
+						
+			the_title(); 
+			 
+			?></h1>
+		</header>
+		
+		<div class="wrapper section-inner">
+	
+	    <?php while ( $children->have_posts() ) : $children->the_post(); ?>
+	
+	        <article id="parent-<?php the_ID(); ?>" class="child-post entry-content">
+	
+						<h2><?php 
+						
+						echo '#'.$post_count.' &ndash; ';
+						
+						$post_count++;
+						
+						the_title(); 
+						
+						// if user is admin
+						if ( current_user_can( 'publish_posts' ) ) {
+							echo '&nbsp;';
+							edit_post_link('🖋️');
+						}
+						
+						 ?></h2>
+	
+	            <div class="content"><?php the_content(); ?></div>
+	
+	        </article>
+					
+					<hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true">
+	
+	    <?php endwhile; ?>
+			
+		</div>
+			
+	</section>
+			
+			
+			
+	
+	<?php endif; 
+	wp_reset_postdata(); 
